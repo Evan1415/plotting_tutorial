aae5718cbe46200aaa7d92bcbbdad2aad9994965
Evan <evan@es-lbl.local>
2020-06-02 17:55:52 +0100

    [ADDFirst commit

---
+++ plotting-template.py
@@ -0,0 +1,26 @@
+# ------------------------------- #
+# ------- Package imports ------- #
+# ------------------------------- #
+
+import numpy as np # imports the entire namespace of functions belonging to numpy
+import matplotlib # imports the entire namespace of functions belonging to matplotlib
+import matplotlib.pyplot as plt # shortens matplotlib.pyplot -> plt
+
+
+# ------------------------- #
+# ------- Plotting  ------- #
+# ------------------------- #
+
+fig,axs = plt.subplots()       # creates a figure object
+x = np.linspace(0, 10, 100)    # creates array from 0->100 with 100 elements in increments
+                               # of 10/100
+
+y = np.sin(x)                  # evaluates sin(x) on the array x
+axs.plot(x, y, 'r')            # plots y(x), 'r': colour red
+axs.set_xlabel('x-axis label') # xlabel
+axs.set_ylabel('y-axis label') # ylabel
+axs.set_title('My first plot') # title
+
+plt.show()                     # displays the figure in the X-window
+
+
